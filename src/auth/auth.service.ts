41371cc17f252fac3590cf25351de6907a839d28
arshiya-geospoc <arshiya.patel@geospoc.com>
2022-04-11 12:29:55 +0530

    Added try catch

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,10 +6,17 @@ export class AuthService {
   constructor(private usersService: UserService) {}
 
   async validateUser(username: string, pass: string): Promise<any> {
-    const user = await this.usersService.findUser(username);
-    if (user && user[0].password == pass){
-        return user;
-    }
-    return null;
+        try
+        {
+            const user = await this.usersService.findUser(username);
+            if (user && user[0].password == pass){
+                return user;
+            }
+            return null;
+        }
+        catch(e)
+        {
+            console.log(e);
+        }
   }
 }
